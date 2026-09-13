ff6564a569db3ec11ca6923aaa6b647e7b9b6b61
amartinsmg <andremindurimg@gmail.com>
2026-09-13 11:31:44 -0300

    refactor: replace anchor tags with button elements for improved accessibility and functionality in header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import "./sass/main.scss";
 
-async function writeName() {
+async function main() {
+
+  // Scroll to top button functionality
+
+  document.querySelectorAll('.to-top').forEach(btn => {
+    btn.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: "smooth" })
+    });
+  });
+
+  // Write name animation functionality
+
   const WhoIAmEl = document.querySelector(
     "#hero-content-text-heading-whoiam"
   ),
@@ -15,10 +26,6 @@ async function writeName() {
     WhoIAmEl.textContent += char;
   }
   TextCursor.classList.add("blinker-effect");
-}
-
-function main() {
-  writeName();
 }
 
 window.addEventListener("load", main);
